43987d7d52fd45aab095dd4c0b845cbf8f4050e6
keralex <kerlis.aguado@correo.unimet.edu.ve>
2019-01-26 08:49:27 -0800

    login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './navigation/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
+  // home
 {
   path:'',
-  // children:[
-  //   {path:'',redirectTo:'/home',pathMatch:'full'},
-  //   // {path:'/home',component}
-  // ],
-  component:HomeComponent
-}
+  children:[
+  {path:'',redirectTo:'/home',pathMatch:'full'},
+   {path:'home',component:LoginComponent}
+   ],  component:HomeComponent
+},
 ];
 
 @NgModule({
